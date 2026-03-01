bfca3b57848ab6e3e8b31ca1cd3f19055509d150
Kamil Warczek <kamilwarczek@gmail.com>
2026-03-01 23:34:03 +0100

    zmiany

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    optimizeDeps: {
+      include: ['astro-leaflet > leaflet'],
+    }
+  },
+
+  integrations: [icon()],
+});
