7b1539c1b0f6d32032409200900d4b856d02dec1
Kamil Warczek <kamilwarczek@gmail.com>
2026-03-12 18:18:02 +0100

    Nowa mapka

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,23 @@ import icon from 'astro-icon';
 
 import netlify from '@astrojs/netlify';
 
+import { webcore } from 'webcoreui/integration';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     optimizeDeps: {
       include: ['astro-leaflet > leaflet'],
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                api: 'modern-compiler'
+            }
+        }
     }
   },
 
-  integrations: [icon()],
+  integrations: [icon(), webcore()],
   adapter: netlify(),
 });
